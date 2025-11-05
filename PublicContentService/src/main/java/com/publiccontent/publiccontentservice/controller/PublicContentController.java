0caede89fa6e32febfe3d9404ef33d0aa4a20807
Danielpinedasalazar <danielpinedasalazar19@gmail.com>
2025-11-05 09:53:35 -0500

    Public content controller

---
+++ PublicContentService/src/main/java/com/publiccontent/publiccontentservice/controller/PublicContentController.java
@@ -0,0 +1,47 @@
+package com.publiccontent.publiccontentservice.controller;
+
+import com.publiccontent.publiccontentservice.dto.PublicContentDTO;
+import com.publiccontent.publiccontentservice.dto.Response;
+import com.publiccontent.publiccontentservice.service.PublicContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/content")
+@RequiredArgsConstructor
+public class PublicContentController {
+
+    private final PublicContentService publicContentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Response<?>> createPublicContent(@RequestBody PublicContentDTO publicContentDTO) {
+        return ResponseEntity.ok(publicContentService.createPublicContent(publicContentDTO));
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
+    public ResponseEntity<Response<List<PublicContentDTO>>> getAllEvents() {
+        return ResponseEntity.ok(publicContentService.getAllEvents());
+    }
+
+    @GetMapping("/approved")
+    public ResponseEntity<Response<List<PublicContentDTO>>> getApprovedPublicContent() {
+        return ResponseEntity.ok(publicContentService.getApprovedPublicContent());
+    }
+
+    @GetMapping("/not-approved")
+    public ResponseEntity<Response<List<PublicContentDTO>>> getApprovedFalsePublicContentById() {
+        return ResponseEntity.ok(publicContentService.getApprovedFalsePublicContentById());
+    }
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('SUPER_ADMIN')")
+    public ResponseEntity<Response<PublicContentDTO>> updateApprovedById(@PathVariable ("id") Long id, @RequestBody PublicContentDTO publicContentDTO ) {
+        return ResponseEntity.ok(publicContentService.updateApprovedById(publicContentDTO, id));
+    }
+}
+
